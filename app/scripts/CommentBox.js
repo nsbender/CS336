c9c4c415e0f797a936dde2b73721c67f277000bf
Nathaniel Bender <nsb2@students.calvin.edu>
2016-11-30 14:20:07 -0500

    Debugging

--- app/scripts/CommentBox.js
+++ app/scripts/CommentBox.js
@@ -1,32 +1,21 @@
 import React from 'react';
-import $ from 'jquery';
 
 import CommentList from './CommentList';
+import $ from 'jquery';
 import CommentForm from './CommentForm';
-import { API_URL, POLL_INTERVAL } from './global';
+
+import { store, ActionTools } from './flux';
 
 module.exports = React.createClass({
     getInitialState: function() {
         return {data: []};
-    },
-    loadCommentsFromServer: function() {
-        $.ajax({
-            url: API_URL,
-            dataType: 'json',
-            cache: false,
-        })
-         .done(function(result){
-             this.setState({data: result});
-         }.bind(this))
-         .fail(function(xhr, status, errorThrown) {
-             console.error(this.props.url, status, errorThrown.toString());
-         }.bind(this));
     },
     handleCommentSubmit: function(comment) {
         var comments = this.state.data;
         comment.id = Date.now();
         var newComments = comments.concat([comment]);
         this.setState({data: newComments});
+    		store.dispatch(ActionTools.addComment(comment));
         $.ajax({
             url: API_URL,
             dataType: 'json',
@@ -41,10 +30,16 @@ module.exports = React.createClass({
              console.error(API_URL, status, errorThrown.toString());
          }.bind(this));
     },
-    componentDidMount: function() {
-        this.loadCommentsFromServer();
-        setInterval(this.loadCommentsFromServer, POLL_INTERVAL);
-    },
+		componentWillMount() {
+    	this.unsubscribe = store.subscribe(() => {
+        this.setState({
+            data: store.getState().data
+        });
+    	});
+		},
+		componentWillUnmount: function() {
+    	this.unsubscribe();
+		},
     render: function() {
         return (
             <div className="CommentBox">
--- app/scripts/CommentEdit.js
+++ app/scripts/CommentEdit.js
@@ -2,85 +2,67 @@ import React from 'react';
 import { Link } from 'react-router';
 import $ from 'jquery';
 
-import { API_URL } from './global';
+import { store, ActionTools, StoreTools } from './flux';
 
 module.exports = React.createClass({
-    getInitialState: function() {
-        return {author: '', text: ''};
-    },
-    componentDidMount: function() {
-        this.loadData();
-    },
-    componentDidUpdate: function(prevProps) {
-        if (this.props.params.id != prevProps.params.id) {
-            this.loadData();
-        }
-    },
-    loadData: function() {
-        $.ajax(API_URL + "/" + this.props.params.id) .done(function(comments) {
-            this.setState(comments[0]);
-        }.bind(this));
-    },
-    handleAuthorChange: function(e) {
-        this.setState({author: e.target.value});
-    },
-    handleTextChange: function(e) {
-        this.setState({text: e.target.value});
-    },
-    contextTypes: {
-        router: React.PropTypes.object
-    },
-    handleUpdate: function() {
-        var updatedComment = {
-            author: this.state.author.trim(),
-            text: this.state.text.trim()
-        }
-        $.ajax({
-            url: API_URL + "/" + this.props.params.id,
-            dataType: 'json',
-            type: 'PUT',
-            contentType:'application/json',
-            data: JSON.stringify(updatedComment)
-        })
-         .done(function(comments){
-             this.context.router.push('/');
-         }.bind(this))
-         .fail(function(xhr, status, errorThrown) {
-             console.error(API_URL, status, errorThrown.toString());
-         }.bind(this));
-    },
-    handleDelete: function() {
-        $.ajax({
-            url: API_URL + "/" + this.props.params.id,
-            type: 'DELETE',
-        })
-         .done(function(comments){
-             this.context.router.push('/');
-         }.bind(this))
-         .fail(function(xhr, status, errorThrown) {
-             console.error(API_URL, status, errorThrown.toString());
-         }.bind(this));
-    },
-    render: function() {
-        return (
-            <div>
-                <form className="CommentForm">
-                    <h1>Comment Edit - {this.state.id}</h1>
-                    <input
-                        type="text"
-                        value={this.state.author}
-                        onChange={this.handleAuthorChange}
-                    />
-                    <input
-                        type="text"
-                        value={this.state.text}
-                        onChange={this.handleTextChange}
-                    />
-                    <button type="button" onClick={this.handleUpdate}>Update</button>
-                    <button type="button" onClick={this.handleDelete}>Delete</button>
-                </form>
-                <Link to='/'>Cancel</Link>
-            </div>
-        );
+  getInitialState: function() {
+    return {author: '', text: ''};
+  },
+  componentDidMount: function() {
+    let commentToEdit = StoreTools.findComment(this.props.params.id, store.getState().data);
+    this.setState({author: commentToEdit.author, text: commentToEdit.text});
+  },
+  componentDidUpdate: function(prevProps) {
+    if (this.props.params.id != prevProps.params.id) {
+      this.loadData();
     }
+  },
+  loadData: function() {
+    $.ajax(API_URL + "/" + this.props.params.id) .done(function(comments) {
+      this.setState(comments[0]);
+    }.bind(this));
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  contextTypes: {
+    router: React.PropTypes.object
+  },
+  handleUpdate: function() {
+    var updatedComment = {
+      author: this.state.author.trim(),
+      text: this.state.text.trim()
+    };
+    store.dispatch(ActionTools.editComment(Number(this.props.params.id), updatedComment));
+    this.context.router.push('/');
+  },
+  handleDelete: function() {
+    store.dispatch(ActionTools.deleteComment(Number(this.props.params.id)));
+    this.context.router.push('/');
+  },
+  render: function() {
+    return (
+      <div>
+      <form className="CommentForm">
+      <h1>Comment Edit - {this.state.id}</h1>
+      <input
+      type="text"
+      value={this.state.author}
+      onChange={this.handleAuthorChange}
+      />
+      <input
+      type="text"
+      value={this.state.text}
+      onChange={this.handleTextChange}
+      />
+      <button type="button" onClick={this.handleUpdate}>Update</button>
+      <button type="button" onClick={this.handleDelete}>Delete</button>
+      </form>
+      <Link to='/'>Cancel</Link>
+      </div>
+    );
+  }
 });
