07e75ed8205be7621746c77a1a2afb280a9d4217
Nathaniel Bender <nsb2@students.calvin.edu>
2016-11-30 13:28:52 -0500

    Updated lab12

--- lab12/app/scripts/Comment.js
+++ lab12/app/scripts/Comment.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Remarkable from 'remarkable';
+import { Link } from 'react-router';
 
 module.exports = React.createClass({
     rawMarkup: function() {
@@ -9,11 +10,12 @@ module.exports = React.createClass({
     },
     render: function() {
         return (
-            <div className="comment">
-                <h2 className="commentAuthor" >
+            <div className="Comment">
+                <h2 className="CommentAuthor" >
                     {this.props.author}
                 </h2>
                 <span dangerouslySetInnerHTML={this.rawMarkup()} />
+		<Link to={'/' + this.props.id}>Edit</Link>
             </div>
         );
     }
--- lab12/server.js
+++ lab12/server.js
@@ -10,65 +10,95 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var MongoClient = require('mongodb').MongoClient;
-var db = 'mongodb://cs336:bjarne@ds017185.mlab.com:17185/cs336';
+ var path = require('path');
+ var express = require('express');
+ var bodyParser = require('body-parser');
+ var app = express();
+ var MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect(db, function (err, db) {
-  if (err) throw err;
-});
+ var db;
+ var APP_PATH = path.join(__dirname, 'dist');
 
-const APP_PATH = '/dist/index.html';
+ app.set('port', (process.env.PORT || 3000));
 
-app.set('port', (process.env.PORT || 3000));
+ app.use('/', express.static(APP_PATH));
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/', express.static(path.join(__dirname, 'dist')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use('*', express.static(APP_PATH));
+ app.use(function(req, res, next) {
+     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader('Cache-Control', 'no-cache');
+     next();
+ });
 
-// Additional middleware which will set headers that we need on each request.
-app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
-    res.setHeader('Access-Control-Allow-Origin', '*');
+ app.get('/api/comments', function(req, res) {
+     db.collection("comments").find({}).toArray(function(err, docs) {
+         if (err) throw err;
+         res.json(docs);
+     });
+ });
 
-    // Disable caching so we'll always get the latest comments.
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-});
+ app.post('/api/comments', function(req, res) {
+     var newComment = {
+         id: Date.now(),
+         author: req.body.author,
+         text: req.body.text,
+     };
+     db.collection("comments").insertOne(newComment, function(err, result) {
+         if (err) throw err;
+         db.collection("comments").find({}).toArray(function(err, docs) {
+             if (err) throw err;
+             res.json(docs);
+         });
+     });
+ });
 
-app.get('/api/comments', function(req, res) {
-    fs.readFile(COMMENTS_FILE, function(err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        res.json(JSON.parse(data));
-    });
-});
+ app.get('/api/comments/:id', function(req, res) {
+     db.collection("comments").find({"id": Number(req.params.id)}).toArray(function(err, docs) {
+         if (err) throw err;
+         res.json(docs);
+     });
+ });
 
-app.post('/api/comments', function(req, res) {
-  var newComment = {
-    id: Date.now(),
-    author: req.body.author,
-    text: req.body.text,
-  };
-  db.collection("comments").insertOne(newComment, function(err, result) {
-    if (err) throw err;
-    var newId = result.insertedId;
-    db.collection("comments").find({}).toArray(function(err, docs) {
-      if (err) throw err;
-      res.json(docs);
-    });
-  });
-});
+ app.put('/api/comments/:id', function(req, res) {
+     var updateId = Number(req.params.id);
+     var update = req.body;
+     db.collection('comments').updateOne(
+         { id: updateId },
+         { $set: update },
+         function(err, result) {
+             if (err) throw err;
+             db.collection("comments").find({}).toArray(function(err, docs) {
+                 if (err) throw err;
+                 res.json(docs);
+             });
+         });
+ });
 
+ app.delete('/api/comments/:id', function(req, res) {
+     db.collection("comments").deleteOne(
+         {'id': Number(req.params.id)},
+         function(err, result) {
+             if (err) throw err;
+             db.collection("comments").find({}).toArray(function(err, docs) {
+                 if (err) throw err;
+                 res.json(docs);
+             });
+         });
+ });
 
-app.listen(app.get('port'), function() {
-    console.log('Server started: http://localhost:' + app.get('port') + '/');
-});
+ // Send all routes/methods not specified above to the app root.
+ app.use('*', express.static(APP_PATH));
+
+ app.listen(app.get('port'), function() {
+     console.log('Server started: http://localhost:' + app.get('port') + '/');
+ });
+
+ // This assumes that the MongoDB password has been set as an environment variable.
+ var mongoURL = 'mongodb://cs336:' +
+                process.env.MONGO_PASSWORD +
+                '@ds017185.mlab.com:17185/cs336';
+ MongoClient.connect(mongoURL, function(err, dbConnection) {
+     if (err) throw err;
+     db = dbConnection;
+ });
